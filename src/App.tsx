196a2c6e9c518d320a43ba8ba009e622826afbc2
Moji Ahmadi <Mo.ahmadi@outlook.com>
2025-09-18 18:24:20 +0100

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css'
 
@@ -7,6 +7,15 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/planets" replace />} />
+        <Route path="/planets" element={<div>Planets</div>} />
+        <Route path="/planets/:id" element={<div>Planet Details</div>} />
+        <Route path="/characters" element={<div>Characters</div>} />
+        <Route path="/characters/:id" element={<div>Character Details</div>} />
+        <Route path="/starships" element={<div>Starships</div>} />
+        <Route path="/starships/:id" element={<div>Starship Details</div>} />
+      </Routes>
     </Router>
   )
 }
